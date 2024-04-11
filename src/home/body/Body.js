979143fd8858fd6da6e6cb993c1a290723d07035
Cameron Chrobocinski <cameronchrobo@gmail.com>
2024-04-11 17:26:23 -0500

    added uuid param support to Body.js

--- src/home/body/Body.js
+++ src/home/body/Body.js
@@ -1,15 +1,21 @@
 import { Box, Grid } from "@mui/material";
+import { useParams } from "react-router-dom";
 import FriendsActivity from "./FriendsActivity";
 import CharacterList from "./character-list/CharacterList";
 
 function Body(props) {
+    const params = useParams();
     return (
         <Box margin={0} padding={0}>
             <Grid container>
                 <Grid item xs={9}>
                     <CharacterList />
                 </Grid>
-                <Grid item xs={3}>
+                <Grid
+                    item
+                    xs={3}
+                    visibility={params.uuid ? "hidden" : "visible"}
+                >
                     <FriendsActivity />
                 </Grid>
             </Grid>
