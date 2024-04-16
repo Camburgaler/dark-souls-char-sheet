880bdfde6bf03277ac602a060cbd29472655f6a8
Cameron Chrobocinski <cameronchrobo@gmail.com>
2024-04-16 10:09:55 -0500

    implemented redux in login

--- src/login/Login.js
+++ src/login/Login.js
@@ -1,13 +1,31 @@
 import { Box, Grid } from "@mui/material";
 import Button from "@mui/material/Button";
-import { BASE_PATH, HOME_PATH } from "../constants";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { HOME_PATH } from "../constants";
+import { setUser } from "../redux/userSlice";
 
 function Login(props) {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const handleLogin = () => {
+        // TODO: Login logic goes here
+        dispatch(
+            setUser(
+                require("../data/accounts.json").find((user) => {
+                    return user.uuid === "126112c3-79d2-43a4-bdaf-179926817396";
+                })
+            )
+        ); // dummy login
+        navigate(HOME_PATH);
+    };
+
     return (
         <Box sx={{ flexGrow: 1 }}>
             <Grid container>
-                <Grid xs={12}>
-                    <Button href={BASE_PATH + HOME_PATH}>LOGIN</Button>
+                <Grid item xs={12}>
+                    <Button onClick={handleLogin}>LOGIN</Button>
                 </Grid>
             </Grid>
         </Box>
