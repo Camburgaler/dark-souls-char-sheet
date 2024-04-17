d46567ab758582b0bbe4389d6364135a8c4a6894
Cameron Chrobocinski <cameronchrobo@gmail.com>
2024-04-17 11:43:24 -0500

    fixed home test

--- src/home/Home.test.js
+++ src/home/Home.test.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Home from './Home';
+import { configureStore } from "@reduxjs/toolkit";
+import { render } from "@testing-library/react";
+import React from "react";
+import { act } from "react-dom/test-utils";
+import { Provider } from "react-redux";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import rootReducer from "../redux/reducers";
+import Home from "./Home";
 
-test('renders Home', () => {
-  const { getByText } = render(
-    <Home />
-  );
+test("renders Home", () => {
+    const store = configureStore({
+        reducer: rootReducer,
+    });
+    act(() => {
+        const { getByText } = render(
+            <BrowserRouter>
+                <Routes>
+                    <Route
+                        path="*"
+                        element={
+                            <Provider store={store}>
+                                <Home />
+                            </Provider>
+                        }
+                    />
+                </Routes>
+            </BrowserRouter>
+        );
+    });
 
-  expect(true).toBeTruthy;
+    expect(true).toBeTruthy;
 });
