3c5572df8cde0154ca99d4ef2c9b7b83c7528f96
Cameron Chrobocinski <cameronchrobo@gmail.com>
2024-04-17 11:41:50 -0500

    added character img dimensions to constants

--- src/constants.js
+++ src/constants.js
@@ -4,3 +4,4 @@ export const HOME_PATH = "/home";
 export const CHARACTER_PATH = "/character";
 export const FRIEND_PATH = "/friend";
 export const CREATE_PATH = "/create";
+export const CHARACTER_IMG_DIMENSIONS = 200;
