4a43636947bf5737f69a4f3aed4eb71bc919a0c6
Cameron Chrobocinski <cameronchrobo@gmail.com>
2024-04-18 11:08:22 -0500

    adjusted add character visibility based on user login

--- src/home/body/character-list/CharacterList.js
+++ src/home/body/character-list/CharacterList.js
@@ -36,44 +36,45 @@ function CharacterList(props) {
                             />
                         );
                     })}
-                    <Button
-                        color="inherit"
-                        sx={{ textTransform: "none" }}
-                        href={BASE_PATH + CREATE_PATH}
-                    >
-                        <Grid
-                            item
-                            padding={1}
-                            margin={1}
-                            xs={"auto"}
-                            sx={{
-                                bgcolor: "gray",
-                                ":hover": {
-                                    border: "1px solid #000000",
-                                    bgcolor: "darkgray",
-                                },
-                            }}
-                            visibility={params.uuid ? "hidden" : "visible"}
+                    <Grid item visibility={user.uuid ? "visible" : "hidden"}>
+                        <Button
+                            color="inherit"
+                            sx={{ textTransform: "none" }}
+                            href={BASE_PATH + CREATE_PATH}
                         >
-                            <Grid container direction={"column"}>
-                                <Grid item xs={9}>
-                                    <Add
-                                        sx={{
-                                            height: CHARACTER_IMG_DIMENSIONS,
-                                            width: CHARACTER_IMG_DIMENSIONS,
-                                        }}
-                                    />
-                                </Grid>
-                                <Grid item xs={3}>
-                                    <Typography
-                                        maxWidth={CHARACTER_IMG_DIMENSIONS}
-                                    >
-                                        New Character
-                                    </Typography>
+                            <Grid
+                                item
+                                padding={1}
+                                margin={1}
+                                xs={"auto"}
+                                sx={{
+                                    bgcolor: "gray",
+                                    ":hover": {
+                                        border: "1px solid #000000",
+                                        bgcolor: "darkgray",
+                                    },
+                                }}
+                            >
+                                <Grid container direction={"column"}>
+                                    <Grid item xs={9}>
+                                        <Add
+                                            sx={{
+                                                height: CHARACTER_IMG_DIMENSIONS,
+                                                width: CHARACTER_IMG_DIMENSIONS,
+                                            }}
+                                        />
+                                    </Grid>
+                                    <Grid item xs={3}>
+                                        <Typography
+                                            maxWidth={CHARACTER_IMG_DIMENSIONS}
+                                        >
+                                            New Character
+                                        </Typography>
+                                    </Grid>
                                 </Grid>
                             </Grid>
-                        </Grid>
-                    </Button>
+                        </Button>
+                    </Grid>
                 </Grid>
             </Grid>
         </Box>
