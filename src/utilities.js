aec830728b5b0e2f5322025f5b087f236e521e35
Cameron Chrobocinski <cameronchrobo@gmail.com>
2024-04-18 14:23:39 -0500

    added friend request methods to utilities

--- src/utilities.js
+++ src/utilities.js
@@ -3,3 +3,19 @@ export const findUsername = (uuid) => {
         return entry.uuid === uuid;
     })?.username;
 };
+
+export const findFriendRequest = (userUuid, friendUuid) => {
+    return require("./data/account_friends.json").find((request) => {
+        return (
+            (request.requestor === userUuid &&
+                request.recipient === friendUuid) ||
+            (request.requestor === friendUuid && request.recipient === userUuid)
+        );
+    });
+};
+
+export const findAcceptedFriendUuid = (userUuid, friendUuid) => {
+    return findFriendRequest(userUuid, friendUuid)?.accepted
+        ? friendUuid
+        : null;
+};
